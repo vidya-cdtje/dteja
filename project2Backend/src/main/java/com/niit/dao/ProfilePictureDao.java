b9603f4dbf0a2c84f640d47f285ae62d2331ff8d
vidhya shankar <38492978+vidya-cdtje@users.noreply.github.com>
2018-07-06 10:47:24 +0530

    Delete ProfilePictureDao.java

--- project2Backend/src/main/java/com/niit/dao/ProfilePictureDao.java
+++
@@ -1,9 +0,0 @@
-package com.niit.dao;
-
-import com.niit.model.ProfilePicture;
-
-public interface ProfilePictureDao {
-	public void save(ProfilePicture profilePicture);
-	public ProfilePicture getProfilePicture(String loginname);
-
-}
